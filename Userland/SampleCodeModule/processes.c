17a39dc3cd90815deaafa8c53a5f79078759b693
stanoira <smartineztanoira@itba.edu.ar>
2023-11-05 17:28:23 -0300

    phylo hot fix

--- Userland/SampleCodeModule/processes.c
+++ Userland/SampleCodeModule/processes.c
@@ -1,5 +1,135 @@
 #include "include/syslib.h"
 
+#define MAX_PHILOSOPHERS 20
+#define EAT_SECONDS 1
+
+#define THINKING 0
+#define EATING 1
+#define HUNGRY 2
+
+sem_t mutexSem;
+sem_t chopstickSem[MAX_PHILOSOPHERS];
+
+int philosopherStates[MAX_PHILOSOPHERS];
+pid_t philosopherPids[MAX_PHILOSOPHERS];
+
+char philosopherNames[MAX_PHILOSOPHERS] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+int philosopherCount = 0;
+
+void takeChopsticks(int philosopher);
+void putChopsticks(int philosopher);
+void checkAvailability(int philosopher);
+int left(int philosopher);
+int right(int philosopher);
+void printState();
+
+void philosopherProcess(int argc, char *argv[]) {
+    int philosopher = atoi(argv[1]);
+
+    while (1) {
+        sleep(EAT_SECONDS);
+        takeChopsticks(philosopher);
+        sleep(EAT_SECONDS);
+        putChopsticks(philosopher);
+    }
+}
+
+void addPhilosopher() {
+    if (philosopherCount >= MAX_PHILOSOPHERS) {
+        fprintf(STDOUT, "Cannot add more philosophers.\n");
+        return;
+    }
+
+    sem_wait(mutexSem);
+
+    philosopherStates[philosopherCount] = THINKING;
+
+    char philosopherName[2];
+    uintToBase(philosopherCount, philosopherName, 10);
+
+    chopstickSem[philosopherCount] = sem_open(philosopherName, 1);
+    if (chopstickSem[philosopherCount] == 0) {
+        fprintf(STDOUT, "Failed to add philosopher.\n");
+        return;
+    }
+
+    char *argv[] = {philosopherName};
+    philosopherPids[philosopherCount] = sys_exec((uint64_t)philosopherProcess, 1, argv);
+
+    fprintf(STDOUT, "Added philosopher %c\n", philosopherNames[philosopherCount]);
+
+    philosopherCount++;
+    sem_post(mutexSem);
+}
+
+void removePhilosopher() {
+    if (philosopherCount == 0) {
+        fprintf(STDOUT, "Failed, the number of philosophers must be greater than 0.\n");
+        return;
+    }
+
+    sem_wait(mutexSem);
+
+    philosopherCount--;
+
+    sem_close(chopstickSem[philosopherCount]);
+    sys_kill(philosopherPids[philosopherCount]);
+
+    fprintf(STDOUT, "Removed philosopher %c\n", philosopherNames[philosopherCount]);
+
+    sem_post(mutexSem);
+}
+
+void finishEating() {
+    while (philosopherCount > 0) {
+        sem_close(chopstickSem[philosopherCount - 1]);
+        sys_kill(philosopherPids[philosopherCount - 1]);
+        philosopherCount--;
+    }
+}
+
+void printState() {
+    for (int i = 0; i < philosopherCount; i++) {
+        philosopherStates[i] == EATING ? fprintf(STDOUT, "E ") : fprintf(STDOUT, ". ");
+    }
+    fprintf(STDOUT, "\n");
+}
+
+void phylo(uint64_t argc, char *argv[]) {
+    fprintf(STDOUT, "Welcome to the dining philosophers' problem!\n");
+
+    mutexSem = sem_open("philosopher_mutex", 1);
+
+    for (int i = 0; i < MAX_PHILOSOPHERS; i++) {
+        chopstickSem[i] = 0;
+    }
+
+    fprintf(STDOUT, "Press 'a' to add a philosopher, 'r' to remove a philosopher, or 'q' to exit.\n");
+
+    char c;
+    while ((c = getChar()) != EOF) {
+        if (c == 'a' || c == 'A') {
+            addPhilosopher();
+        } else if (c == 'r' || c == 'R') {
+            removePhilosopher();
+        } else if (c == 'q' || c == 'Q') {
+            break;
+        }
+    }
+
+    finishEating();
+
+    for (int i = 0; i < MAX_PHILOSOPHERS; i++) {
+        if (chopstickSem[i] != 0) {
+            sem_close(chopstickSem[i]);
+        }
+    }
+
+    sem_close(mutexSem);
+
+    sys_exit(0);
+}
+
 #define MAX_PHYLOSOPHERS 20
 #define EAT_SECONDS 1
 
@@ -14,7 +144,7 @@ int phyloQty = 0;
 
 int state[MAX_PHYLOSOPHERS];
 pid_t phyloPids[MAX_PHYLOSOPHERS];
-char phyloNames[MAX_PHYLOSOPHERS] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+char phyloNamphyloNameses[MAX_PHYLOSOPHERS] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
 
 void takeChopsticks(int index);
 void putChopsticks(int index);
